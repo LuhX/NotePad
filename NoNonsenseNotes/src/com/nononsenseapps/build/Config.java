6ef86d37c752b26fe596bbbd229beb51537422a9
LuhX <privateluiza@gmail.com>
2013-06-20 10:38:29 -0300

    Update Config.java

--- NoNonsenseNotes/src/com/nononsenseapps/build/Config.java
+++ NoNonsenseNotes/src/com/nononsenseapps/build/Config.java
@@ -6,7 +6,7 @@ package com.nononsenseapps.build;
  */
 public class Config {
 	public final static boolean LOGGING = true;
-	public final static String GTASKS_API_KEY = "Put your key here";
+	public final static String GTASKS_API_KEY = "AIzaSyAdfYT0NZUZEfNVyqDNnl6y6Th1qbCQOjk";
 	public final static String DROPBOX_APP_KEY = "Put your key here";
 	public final static String DROPBOX_APP_SECRET = "Put your key here";
 }
